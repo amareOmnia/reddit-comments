5f174b85b0f7b5e47a396085fb1ee39a5f3122c2
Cooper Lewis <thatcooperlewis@gmail.com>
2018-10-31 10:41:45 -0400

    Header created

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="App-header">
+          <img className='header__logo' src="./images/reddit-logo.png" alt="reddit logo"/>
+          <div className='header__text'>
+            <h1 className='title'>Comment Search</h1>
+            <h2 className="subtitle">A browser of all reddit comments ever</h2>
+          </div>
+          <div className="credits">
+            <h3 className="">Built by Cooper Lewis</h3>
+            <h3 className="">Not affiliated with Reddit.com</h3>
+          </div>
+        </div>
+
+        <div className="App-body">
+          this is the body
+        </div>
       </div>
     );
   }
