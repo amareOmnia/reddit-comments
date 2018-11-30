302960e68ee5ad8458df95fee7b755ef533ac178
Cooper Lewis <thatcooperlewis@gmail.com>
2018-11-30 11:14:41 -0500

    remove old files

--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
 subTest = '''SELECT body,author,score 
 FROM [fh-bigquery:reddit_comments.2017_04] 
-WHERE score>500 
+WHERE score>50 
 AND body!='[removed]' 
 AND body!='[deleted]' 
 AND subreddit='cats' OR subreddit = 'feetsies'
